3f88ea9fd343d5a39c697c9bce776c067c6cc3cf
Igor Rončević <ironcev@hotmail.com>
2017-10-18 23:21:16 +0200

    losing-weight: Adds comparison for constructors and finalizers

---
+++ LosingWeightWithCSharp7+/Snippets/ExpressionBodiedMembersWithConstructorsFinalizersAndAccessors/CompareConstructors.cs
@@ -0,0 +1,19 @@
+﻿namespace LosingWeightWithCSharp7Plus.Snippets.ExpressionBodiedMembersWithConstructorsFinalizersAndAccessors
+{
+    public class ConstructorBefore
+    {
+        public ConstructorBefore(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get; }        
+    }
+
+    public class ConstructorAfter
+    {
+        public ConstructorAfter(int id) => Id = id;
+
+        public int Id { get; }
+    }
+}
---
+++ LosingWeightWithCSharp7+/Snippets/ExpressionBodiedMembersWithConstructorsFinalizersAndAccessors/CompareFinalizers.cs
@@ -0,0 +1,19 @@
+﻿namespace LosingWeightWithCSharp7Plus.Snippets.ExpressionBodiedMembersWithConstructorsFinalizersAndAccessors
+{
+    public class FinalizerBefore
+    {
+        ~FinalizerBefore()
+        {
+            ReleaseUnmanagedResources();
+        }
+
+        private void ReleaseUnmanagedResources() { }        
+    }
+
+    public class FinalizerAfter
+    {
+        ~FinalizerAfter() => ReleaseUnmanagedResources();
+
+        private void ReleaseUnmanagedResources() { }
+    }
+}
