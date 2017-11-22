6a3189a887c0e7ab5a61fe1f16e83fbfd0f74745
Igor Rončević <ironcev@hotmail.com>
2017-11-22 16:51:57 +0100

    losing-weight: Adds CompilerDefaultExpressionIssue

---
+++ LosingWeightWithCSharp7+/Snippets/DefaultExpressions/CompilerDefaultExpressionIssue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LosingWeightWithCSharp7Plus.Snippets.DefaultExpressions
+{
+    // Example taken from (and slightly adapted):
+    //      https://github.com/dotnet/csharplang/issues/970
+    public class CompilerDefaultExpressionIssue
+    {
+        public void ShowIssue()
+        {
+            int? x = default; // null
+            int y = default; // 0
+
+            Console.WriteLine(x); // null
+            Console.WriteLine(y); // 0
+
+            ProblematicMethod(); // 0 !!
+            ProblematicMethod(default); // null !!
+        }
+
+        static void ProblematicMethod(int? x = default) // 0 !!
+        {
+            Console.WriteLine(x);
+        }
+
+        public static void TestMe()
+        {
+            var test = new CompilerDefaultExpressionIssue();
+
+            test.ShowIssue();
+        }
+    }
+}
