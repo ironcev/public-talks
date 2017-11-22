8ac4e1f33cafab3245db01641412344fb614baa2
Igor Rončević <ironcev@hotmail.com>
2017-11-22 16:45:19 +0100

    losing-weight: Renames DefaultIssue to SimplifyDefaultExpressionIssue

--- LosingWeightWithCSharp7+/Snippets/DefaultExpressions/SimplifyDefaultExpressionIssue.cs
+++ LosingWeightWithCSharp7+/Snippets/DefaultExpressions/SimplifyDefaultExpressionIssue.cs
@@ -2,21 +2,21 @@
 
 namespace LosingWeightWithCSharp7Plus.Snippets.DefaultExpressions
 {
-    public class DefaultIssue
+    public class SimplifyDefaultExpressionIssue
     {
         public bool IsEqualToDefaultWithT<T>(T value)
         {
-            return value.Equals(default(T)) ? true : false;
+            return value.Equals(default(T));
         }
 
         public bool IsEqualToDefaultWithoutT<T>(T value)
         {
-            return value.Equals(default) ? true : false;
+            return value.Equals(default);
         }
 
         public static void TestMe()
         {
-            var test = new DefaultIssue();
+            var test = new SimplifyDefaultExpressionIssue();
 
             Console.WriteLine( test.IsEqualToDefaultWithT(0) ); // Prints true.
             Console.WriteLine( test.IsEqualToDefaultWithoutT(0) ); // Prints false!
